bbff107d25bd916ffbd7b0ef722cd7f74c52d537
parkernowicki <parkernowicki@gmail.com>
2021-04-10 19:16:54 -0700

    Working on ticket purchase for customers...

--- venv/Scripts/init1.py
+++ venv/Scripts/init1.py
@@ -25,7 +25,7 @@ def viewflights():
 	#cursor used to send queries
 	cursor = conn.cursor()
 	#executes query
-	query = 'SELECT * FROM flight WHERE depart_date >= CURRENT_DATE() AND depart_date >= CURRENT_TIME()'
+	query = 'SELECT * FROM flight WHERE depart_date > CURRENT_DATE() OR (depart_date = CURRENT_DATE() AND depart_time > CURRENT_TIME())'
 	cursor.execute(query)
 	#stores the results in a variable
 	data = cursor.fetchall()
@@ -40,7 +40,7 @@ def flightBySource():
 	airport = request.form['sourceairport']
 
 	cursor = conn.cursor()
-	query = 'SELECT * FROM flight WHERE depart_airport = %s AND depart_date >= CURRENT_DATE() AND depart_date >= CURRENT_TIME()'
+	query = 'SELECT * FROM flight WHERE depart_airport = %s AND (depart_date > CURRENT_DATE() OR (depart_date = CURRENT_DATE() AND depart_time > CURRENT_TIME()))'
 	cursor.execute(query, (airport))
 	data = cursor.fetchall()
 	cursor.close()
@@ -52,7 +52,7 @@ def flightByDest():
 	airport = request.form['destairport']
 
 	cursor = conn.cursor()
-	query = 'SELECT * FROM flight WHERE arrive_airport = %s AND depart_date >= CURRENT_DATE() AND depart_date >= CURRENT_TIME()'
+	query = 'SELECT * FROM flight WHERE arrive_airport = %s AND (depart_date > CURRENT_DATE() OR (depart_date = CURRENT_DATE() AND depart_time > CURRENT_TIME()))'
 	cursor.execute(query, (airport))
 	data = cursor.fetchall()
 	cursor.close()
@@ -64,7 +64,7 @@ def flightByDate():
 	date = request.form['departdate']
 
 	cursor = conn.cursor()
-	query = 'SELECT * FROM flight WHERE depart_date = %s AND depart_date >= CURRENT_DATE() AND depart_date >= CURRENT_TIME()'
+	query = 'SELECT * FROM flight WHERE depart_date = %s AND (depart_date > CURRENT_DATE() OR (depart_date = CURRENT_DATE() AND depart_time > CURRENT_TIME()))'
 	cursor.execute(query, (date))
 	data = cursor.fetchall()
 	cursor.close()
@@ -92,7 +92,7 @@ def loginCustAuth():
 	password = request.form['password']
 
 	cursor = conn.cursor()
-	query = 'SELECT * FROM customer WHERE email = %s and password = %s'
+	query = 'SELECT * FROM customer WHERE email = %s and password = MD5(%s)'
 	cursor.execute(query, (email, password))
 	data = cursor.fetchone()
 	cursor.close()
@@ -113,7 +113,7 @@ def loginAgentAuth():
 	password = request.form['password']
 
 	cursor = conn.cursor()
-	query = 'SELECT * FROM bookingagent WHERE email = %s and password = %s'
+	query = 'SELECT * FROM bookingagent WHERE email = %s and password = MD5(%s)'
 	cursor.execute(query, (email, password))
 	data = cursor.fetchone()
 	cursor.close()
@@ -132,7 +132,7 @@ def loginStaffAuth():
 	password = request.form['password']
 
 	cursor = conn.cursor()
-	query = 'SELECT * FROM airlinestaff WHERE username = %s and user_password = %s'
+	query = 'SELECT * FROM airlinestaff WHERE username = %s and user_password = MD5(%s)'
 	cursor.execute(query, (username, password))
 	data = cursor.fetchone()
 	cursor.close()
@@ -152,9 +152,95 @@ def homecust():
     cursor = conn.cursor()
     query = 'SELECT * FROM customer WHERE email = %s'
     cursor.execute(query, (email))
-    data = cursor.fetchone() 
+    userdata = cursor.fetchone()
+    query = """
+			SELECT
+				flight.airline_name, flight.flight_num, flight.depart_date, flight.depart_time, flight.arrive_date, flight.arrive_time,
+				flight.flight_status, flight.base_price, flight.depart_airport, flight.arrive_airport, flight.airplane_ID
+			FROM
+				customer, cust_purchases, ticket NATURAL JOIN flight
+			WHERE
+				customer.email = %s AND customer.email = cust_purchases.cust_email AND cust_purchases.ticket_ID = ticket.ID AND
+				(flight.depart_date > CURRENT_DATE() OR (flight.depart_date = CURRENT_DATE() AND flight.depart_time > CURRENT_TIME()))
+			"""
+    cursor.execute(query, (email))
+    flightdata = cursor.fetchall()
     cursor.close()
-    return render_template('homecust.html', customer=data)
+    return render_template('homecust.html', customer=userdata, flights=flightdata)
+
+#Customer can purchase a ticket
+@app.route('/purchaseTicketCust')
+def purchaseTicketCust():
+	
+    cursor = conn.cursor()
+    query = 'SELECT * FROM flight WHERE depart_date > CURRENT_DATE() OR (depart_date = CURRENT_DATE() AND depart_time > CURRENT_TIME())'
+    cursor.execute(query)
+    data = cursor.fetchall()
+    cursor.close()
+    return render_template('purchaseTicketCust.html', flights=data)
+
+#Authenticates customer ticket purchase, doesnt work quite yet...
+@app.route('/purchaseTicketCustAuth', methods=['GET', 'POST'])
+def purchaseTicketCustAuth():
+
+	airline = request.form['airline']
+	flightno = request.form['flightno']
+	departdate = request.form['departdate']
+	departtime = request.form['departtime']
+	cardno = request.form['cardno']
+	cardtype = request.form['cardtype']
+	cardexp = request.form['cardexp']
+	ticketid = request.form['ticketid']
+
+	cursor = conn.cursor()
+	query = 'SELECT * FROM ticket WHERE ID = %s'
+	cursor.execute(query, (ticketid))
+	ticketexists = cursor.fetchone()
+
+	query = """
+				SELECT *
+				FROM
+					flight
+					NATURAL JOIN(
+					SELECT
+						airline_name, depart_date, depart_time, flight_num, COUNT(ID) AS ticketCount
+					FROM
+						ticket
+					GROUP BY
+						airline_name, depart_date, depart_time, flight_num
+					) AS ticketsPerFlight,
+					airplane
+				WHERE
+					flight.airline_name = %s AND flight.flight_num = %s AND flight.depart_date = %s AND flight.depart_time = %s AND
+					flight.airline_name = airplane.airline_name AND flight.airplane_ID = airplane.airplane_ID AND airplane.seat_amount = ticketsPerFlight.ticketCount
+			"""
+	cursor.execute(query, (airline, flightno, departdate, departtime))
+	seatsfull = cursor.fetchone()
+
+	query = 'SELECT name FROM customer WHERE email = %s'
+	cursor.execute(query, (session['email']))
+	name = cursor.fetchone()
+
+	query = 'SELECT name FROM customer WHERE email = %s'
+	cursor.execute(query, (session['email']))
+	name = cursor.fetchone()
+
+	error = None
+	if(ticketexists):
+		error = "Existing ticket; try another ID"
+		cursor.close()
+		return render_template('purchaseTicketCust.html', error=error)
+	elif(seatsfull):
+		error = "Seats are filled for that flight"
+		cursor.close()
+		return render_template('purchaseTicketCust.html', error=error)
+	else:
+		soldprice = 40.00 #fixing this later
+		ins = 'INSERT INTO ticket VALUES(%s, 40.00, %s, %s, %s, %s, CURRENT_DATE(), CURRENT_TIME(), %s, %s, %s, %s)'
+		cursor.execute(ins, (ticketid, cardno, cardtype, cardexp, name, airline, flightno, departdate, departtime))
+		conn.commit()
+		cursor.close()
+		return redirect(url_for('homecust'))
 
 #Home page for agent
 @app.route('/homeagent')
@@ -199,7 +285,17 @@ def registerstaff():
 @app.route('/registerCustAuth', methods=['GET', 'POST'])
 def registerCustAuth():
 	email = request.form['email']
+	name = request.form['name']
 	password = request.form['password']
+	buildingno = request.form['buildingno']
+	street = request.form['street']
+	city = request.form['city']
+	state = request.form['state']
+	phonenumber = request.form['phonenumber']
+	passportno = request.form['passportno']
+	passportexp = request.form['passportexp']
+	passportcountry = request.form['passportcountry']
+	dateofbirth = request.form['dateofbirth']
 
 	cursor = conn.cursor()
 	query = 'SELECT * FROM customer WHERE email = %s'
@@ -212,8 +308,9 @@ def registerCustAuth():
 		return render_template('registercust.html', error=error)
 	else:
 		#Might want to require these NULL attributes to be filled out later...
-		ins = 'INSERT INTO customer VALUES(%s, NULL, %s, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)'
-		cursor.execute(ins, (email, password))
+		ins = 'INSERT INTO customer VALUES(%s, %s, MD5(%s), %s, %s, %s, %s, %s, %s, %s, %s, %s)'
+		cursor.execute(ins, (email, name, password, buildingno, street, city, state,
+								phonenumber, passportno, passportexp, passportcountry, dateofbirth))
 		conn.commit()
 		cursor.close()
 		session['email'] = email
@@ -224,6 +321,7 @@ def registerCustAuth():
 def registerAgentAuth():
 	email = request.form['email']
 	password = request.form['password']
+	agentid = request.form['agentid']
 
 	cursor = conn.cursor()
 	query = 'SELECT * FROM bookingagent WHERE email = %s'
@@ -234,8 +332,8 @@ def registerAgentAuth():
 		error = "Existing agent; try another email address"
 		return render_template('registeragent.html', error=error)
 	else:
-		ins = 'INSERT INTO bookingagent VALUES(%s, %s, NULL, NULL)'
-		cursor.execute(ins, (email, password))
+		ins = 'INSERT INTO bookingagent VALUES(%s, MD5(%s), %s, 0.00)'
+		cursor.execute(ins, (email, password, agentid))
 		conn.commit()
 		cursor.close()
 		session['email'] = email
@@ -246,6 +344,10 @@ def registerAgentAuth():
 def registerStaffAuth():
 	username = request.form['username']
 	password = request.form['password']
+	firstname = request.form['firstname']
+	lastname = request.form['lastname']
+	dateofbirth = request.form['dateofbirth']
+	airline = request.form['airline']
 
 	cursor = conn.cursor()
 	query = 'SELECT * FROM airlinestaff WHERE username = %s'
@@ -256,8 +358,8 @@ def registerStaffAuth():
 		error = "Existing staff; try another username"
 		return render_template('registerstaff.html', error=error)
 	else:
-		ins = 'INSERT INTO airlinestaff VALUES(%s, %s, NULL, NULL, NULL, NULL)'
-		cursor.execute(ins, (username, password))
+		ins = 'INSERT INTO airlinestaff VALUES(%s, MD5(%s), %s, %s, %s, %s)'
+		cursor.execute(ins, (username, password, firstname, lastname, dateofbirth, airline))
 		conn.commit()
 		cursor.close()
 		session['username'] = username
